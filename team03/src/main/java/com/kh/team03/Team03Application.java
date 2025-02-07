1bd566b107a3f22f0d58faa99263021c9a2ffc73
Hong <abc@yahoo.com>
2025-02-07 12:10:07 +0900

    250207: spring 설치

---
+++ team03/src/main/java/com/kh/team03/Team03Application.java
@@ -0,0 +1,13 @@
+package com.kh.team03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team03Application.class, args);
+	}
+
+}
