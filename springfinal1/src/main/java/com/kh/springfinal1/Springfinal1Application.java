d700c145b1fb5d50ae9feb36544ea5503bef397e
Hong <abc@yahoo.com>
2025-02-07 12:39:14 +0900

    20250207 spring dev에 올림

---
+++ springfinal1/src/main/java/com/kh/springfinal1/Springfinal1Application.java
@@ -0,0 +1,13 @@
+package com.kh.springfinal1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springfinal1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springfinal1Application.class, args);
+	}
+
+}
---
+++ springfinal1/src/test/java/com/kh/springfinal1/Springfinal1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.springfinal1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springfinal1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
