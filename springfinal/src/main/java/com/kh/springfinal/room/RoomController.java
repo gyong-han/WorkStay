6b07507d48d4cea31e0466e880c6bdc68ba07c34
dldprma <amrpdld@gmail.com>
2025-02-14 16:06:58 +0900

    250214 : Docs 백엔드 문서 init

---
+++ springfinal/src/main/java/com/kh/springfinal/room/RoomController.java
@@ -0,0 +1,9 @@
+package com.kh.springfinal.room;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class RoomController {
+}
---
+++ springfinal/src/main/java/com/kh/springfinal/stayReservation/RoomReservationVo.java
@@ -0,0 +1,20 @@
+package com.kh.springfinal.stayReservation;
+
+import lombok.Data;
+
+@Data
+public class RoomReservationVo {
+private Long no;
+private Long memberNo;
+private Long paymentNo;
+private Long roomNo;
+private Long statusNo;
+private String checkIn;
+private String checkOut;
+private String adult;
+private String child;
+private String baby;
+private String request;
+private String amount;
+private String reservationDate;
+}
