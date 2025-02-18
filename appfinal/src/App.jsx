64b9b4bfa8b1c3c0ec74435134d9f09ebf0d78ff
DESKTOP-IRG8GNP\user1 <kims8479@naver.com>
2025-02-18 09:23:20 +0900

    20250218 FEAT : 공간 상세보기 데이터 SLICE 생성 구현 store 적용

--- appfinal/src/App.jsx
+++ appfinal/src/App.jsx
@@ -42,7 +42,7 @@ function App() {
             }
           />
           <Route
-            path="/findspace"
+            path="/findspace/*"
             element={
               <Layout>
                 <FindSpace />
--- appfinal/src/redux/store.jsx
+++ appfinal/src/redux/store.jsx
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import stationReducer from "./stationSlice";
 import terminalReducer from "./terminalSlice";
 import counterReducer from "./counterSlice";
+import spaceReducer from "./spaceSlice";
 
 const Store = configureStore({
   reducer: {
     station: stationReducer,
     terminal: terminalReducer,
     counter: counterReducer,
+    space : spaceReducer,
   },
 });
 
