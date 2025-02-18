d1e196744d0844b35fe10bf8fcfc7d5d4a111902
dldprma <amrpdld@gmail.com>
2025-02-18 10:02:53 +0900

    250218 feat : 리스트 기능 추가

---
+++ springfinal/src/main/java/com/kh/springfinal/stay/StayAttachmentVo.java
@@ -0,0 +1,14 @@
+package com.kh.springfinal.stay;
+
+import lombok.Data;
+
+@Data
+public class StayAttachmentVo {
+private Long no;
+private Long stayNo;
+private String originName;
+private String changeName;
+private String filePath;
+private String delYn;
+private String thumbnail;
+}
--- springfinal/src/main/java/com/kh/springfinal/stay/StayService.java
+++ springfinal/src/main/java/com/kh/springfinal/stay/StayService.java
@@ -1,4 +1,29 @@
 package com.kh.springfinal.stay;
 
+import com.kh.springfinal.space.AttachmentVo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class StayService {
+    private final StayMapper stayMapper;
+
+    public List<StayVo> getFindStayAll() {
+        List<StayVo> voList = stayMapper.getFindStayAll();
+
+        return voList;
+    }
+
+    public List<AttachmentVo> stayGetAttachmentList() {
+        return stayMapper.stayGetAttachmentList();
+    }
+
+//    public StayVo getFindStayByNo(Long no) {
+//
+//    }
 }
